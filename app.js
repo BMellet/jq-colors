a7f91b087fcc7a2ff7d7f099fe3d738e4de6cb6b
BMellet <benzero.m@gmail.com>
2017-12-08 10:45:40 +0100

    success

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// good luck !
+$(document).ready(function()
+{
+	
+	
+	$('.color').each(function(){
+
+		var color = $(this).attr('data-color');
+		this.style.backgroundColor = color;
+	});
+
+	
+
+	$('.color').click(function()
+	{
+		var color = $(this).attr('data-color');
+		var check = $('#modify-texte').is(':checked')
+
+		if (check)
+		{
+			$(".main").css("color",color);
+		}
+		else
+		{
+			$(".main").css("background-color",color);
+		}
+
+
+	});
+
+});// good luck !
